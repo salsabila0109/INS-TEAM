4e15861516bdc1e9793ae8b6f6b460fcb2a5bd01
IzzaIrena <izzairena03@gmail.com>
2026-06-17 12:58:28 +0800

    debug env

--- backend/server.js
+++ backend/server.js
@@ -68,6 +68,10 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 // database
 db.authenticate()
